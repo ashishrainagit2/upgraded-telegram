7c1716e2e1fd037277cd3370c7f44cd29d13cb44
ashish <ashish95614@gmail.com>
2026-09-18 15:38:39 +0530

    changed heading

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import Counter from './Counter.jsx';
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Simple Counter App for devops purpose</h1>
       <Counter />
     </div>
   );
